2855087a8b1008b1c307978142925e8c29ea0cbd
Dominik <dominikuchyra16@gmail.com>
2025-05-17 12:18:41 +0200

    Composite design pattern added

--- main.cpp
+++ main.cpp
@@ -2,76 +2,99 @@
 #include <vector>
 using namespace std;
 
-//Aplikacja estymujaca zuzycie energii w budynku
+// Aplikacja estymujaca zuzycie energii w budynku
+
+class Component {
+public:
+    string name;
+
+    virtual void display() {}
+};
+
+class Building : public Component {
+private:
+    vector<Component*> rooms;
+
+public:
+    Building(string n) {
+        name = n;
+    }
+
+    void add(Component* component) {
+        rooms.push_back(component);
+    }
+
+    void traverse_rooms() {
+        for (int i = 0; i < rooms.size(); i++) {
+            rooms.at(i)->display(); 
+        }
+    }
+};
+
+class Device {
+	private:
+		string name;
+		int power;
+		int hours;
+		string type;
+		
+	public:
+		Device(string name, int power) {
+			this->name = name;
+			this->power = power; 
+		}
+		
+		void getName() {
+			cout << this->name;
+		}
+		
+};
 
 
-/**
- * The base Component class declares common operations for both simple and
- * complex objects of a composition.
- */
-//class Component {
-//	public:
-//		string name;
-//		
-//		virtual void display() {}
-//		
-//};
-//
-//
-//class Building : public Component {
-//	private:
-//		vector<Component*> rooms;
-//		
-//	public:
-//		Building(string n) {
-//			name = n;
-//		}
-//		void add(Component* component) {
-//			rooms.push_back(component);
-//		}
-//		
-//
-//		void traverse_rooms() {
-//			for(int i = 0; i < rooms.size(); i++) {
-//			//	cout << "Room: " << rooms.at(i)->display() << endl;
-//			}
-//		}
-//};
-//
-//class Room: public Component {
-//	
-//	public:
-//	
-//		Room(string n) {
-//			name = n;
-//		}
-//		
-//		void display() override {
-//			cout << "Pokoj: " << name << endl;
-//			cout << "Nie ma na razie urzdzen";
-//		}
-//};
+class Room : public Component {
+	private:
+		vector<Device*> devices;
+	
+	public:
+    Room(string n) {
+        name = n;
+    }
 
+    void display() override {
+        cout << "Pokoj: " << name << endl;
+        if(devices.empty()) {
+             cout << "Nie ma na razie urzadzen" << endl;	
+		} else {
+			display_devices();
+		}
 
-
-
+    }
+    
+    void addDevice(Device* device) {
+    	devices.push_back(device);
+	}
+	
+	void display_devices() {
+		for(int i = 0; i < devices.size(); i++) {
+			devices.at(i)->getName();
+		}
+	}
+};
 
 
 int main() {
-	
-	cout << "Aplikacja estymujaca zuzycie energii w budynku";
-	
-//	Room* pokoj1 = new Room("Kuchnia");
-//	
-//	Building* building1 = new Building("Dom");
-//	
-//	building1->add(pokoj1);
-//
-//	
-//	building1->traverse_rooms();
-//	
-//	delete pokoj1;
-//	delete building1;
-//	
-	return 0;
+    cout << "Aplikacja estymujaca zuzycie energii w budynku" << endl;
+
+    Room* pokoj1 = new Room("Kuchnia");
+    Building* building1 = new Building("Dom");
+    Device* device1 = new Device("Piekarnik", 123);
+
+    building1->add(pokoj1);
+    
+    pokoj1->addDevice(device1);
+
+    building1->traverse_rooms();
+
+	getchar();
+	getchar();
 }
